6e592b45b8d789c84f23be49aae02bc8e22d6864
sahar <saharhallaji.dev@gmail.com>
2024-11-16 00:15:18 +0330

    update(user model): role enum has added to the user model.

--- models/users.go
+++ models/users.go
@@ -5,11 +5,17 @@ import (
 	"gorm.io/gorm"
 )
 
+const (
+	RoleAdmin      = "admin"
+	RoleUser       = "user"
+	RoleSuperAdmin = "super-admin"
+)
+
 // Users struct definition as before
 type Users struct {
 	ID          string `gorm:"type:uuid;primary_key;"`
 	Telegram_ID string `gorm:"type:varchar(10)"`
-	Role        string `gorm:"type:varchar(15)"`
+	Role        string `gorm:"type:enum('admin', 'user', 'super-admin');not null;default:'user'"`
 
 	Filers []Filters `gorm:"foreignKey:USER_ID"`
 }
